02df09c1182d779e031a7449c253527c4499d46f
anicazulch <31353283+anicazulch@users.noreply.github.com>
2017-09-08 10:46:35 -0700

    Lab 3 Day 2.5

--- Karel-Lab/lab_3/individual_lab/Problem3.java
+++ Karel-Lab/lab_3/individual_lab/Problem3.java
@@ -7,15 +7,36 @@ public class Problem3 extends Problem2
 {
    
     /**
-     * Constructor for objects of class Problem1
+     * Constructor for objects of class Problem3
      */
     public Problem3(int st, int av, Direction dir, int beep)
     {
         super(st, av, dir, beep);
     }
 
-    public void carpetRooms(){
-        
+    public void enterRoom(){
+        turnLeft();
+        for (int i = 0; i <4; i++) {
+        if (frontIsClear()){
+           move();
+           checkRoom();
+           if (checkLeftWall() && checkRightWall() && checkCeiling()){
+            carpet();
+           }
+        }
+    
     }
+   } 
+   public void exitRoom(){
+        faceSouth();
+        while (frontIsClear()){
+            move();
+        }
+    }
+   public void checkRoom(){
+       checkLeftWall();
+       checkRightWall();
+       checkCeiling(); 
+   }
 }
 
